73caa98705a686b8c2208734128069293ed2a7e7
Streetmeat25 <kotpersikchannel@gmail.com>
2022-05-28 16:09:59 +1000

    styles

---
+++ client/components/Products/products.js
@@ -0,0 +1,31 @@
+class Products{
+
+    render(){
+        let htmlCatalog = '';
+        
+        CATALOG.forEach(({id, name, price, img}) =>{
+
+            htmlCatalog += `
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
+                    <img class="products-element__img" src="${img}"/>
+                    <span class="products-element__price">
+                        🏷️ ${price.toLocaleString()} RUB
+                    </span>
+                    <button class="products-element__btn"> Добавить в корзину</button>
+                </li>
+            `;
+        });
+
+        const html = `
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>        
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
