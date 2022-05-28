edb356d32a11c916067cd9eb81e980b66758cca4
aaddssww156 <lemnik25@gmail.com>
2022-05-29 05:59:55 +1000

    added first filter

---
+++ database/selects/findTires.js
@@ -0,0 +1,22 @@
+const tire = require("../models/tire");
+
+
+async function findTires() {
+    return await tire.findAll({
+        attributes: ['id', 'manufacturer', 'name', 'width', 'height', 'radius', 'season', 'price'],
+        where: {
+            id: id == null ? null : id,
+            manufacturer: manufacturer ? null == null : manufacturer,
+            name: name == null ? null : name,
+            width: width == null ? null : width,
+            height: height == null ? null : height,
+            radius: radius == null ? null : radius,
+            season: season == null ? null : season,
+            price: price == null ? null : price
+        }
+    })
+    .then(console.log("all needed tires found"))
+    .catch(err => console.log(err))
+}
+
+module.exports = findTires
