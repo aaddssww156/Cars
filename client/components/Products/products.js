dafbc8f2a7b72224783d19dcd30cbea4fed40bee
Streetmeat25 <kotpersikchannel@gmail.com>
2022-05-29 17:55:31 +1000

    rand pic

--- client/components/Products/products.js
+++ client/components/Products/products.js
@@ -3,8 +3,9 @@ class Products{
         this.classNameActive = 'products-element__btn_active';
         this.labelAdd = 'Добавить в корзину';
         this.labelRemove = 'Удалить из корзины';
+        this.imgArray = ['https://vladivostok.td-kama.com/upload/resize_cache/iblock/1c7/440_440_1/02_Bosco%20AT_V-237.png','https://autodromo.ru/sites/default/files/styles/node/public/articles/23-11.png?itok=VsRkwED2','https://grosta.ru/upload/iblock/ca3/ca3b2ffae7da9d4bfdb532467b1ce30d.jpg','https://ae04.alicdn.com/kf/U46a6a362d3e4416bb0ba10ef6bb53a5ax/Pirelli-225-60-17-v-99-powergy-summer-tire.jpg_480x480xzq55.jpg'];
     }
-
+    
     handleSetLocationStorage(element, id){
         const { pushProduct, products } = localStorageUtil.putProducts(id);
         
@@ -26,7 +27,7 @@ class Products{
         CATALOG.forEach(({id, manufacturer, name, width, height, radius, season, price, img }) => {
             let activeClass = '';
             let activeText = '';
-
+            img = this.imgArray[Math.floor(Math.random() * this.imgArray.length)];
             if (productsStore.indexOf(id) === -1) {
                 activeText = this.labelAdd;                
             } else {
